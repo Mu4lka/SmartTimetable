9b4e4b393ecadc0b28f16a64a10479a3e3d17260
Mu4lka <110475115+Mu4lka@users.noreply.github.com>
2024-04-23 11:43:34 +0700

    1.5.4.b
    Added business logic for notifying about changes in an employee’s schedule

--- utils/services/notification_system/notify_workers/notify_change.py
+++ utils/services/notification_system/notify_workers/notify_change.py
@@ -4,15 +4,38 @@ from loader import bot, worker_table
 from utils.methods import make_form
 
 
+def get_differences(current_element: list, element: list):
+    element.pop(0)
+    max_length = 7
+    current_element += ["Нет"] * (max_length - len(current_element))
+    element += ["Нет"] * (max_length - len(element))
+    differences = {}
+    i = 0
+    for item1, item2 in zip(element, current_element):
+        if item1 != item2 and (not (item1 == "Нет" and item2 == "")):
+            if item1 == "":
+                item1 = "Нет"
+            if item2 == "":
+                item2 = "Нет"
+            differences.update(
+                {constants.week_abbreviated[i]: f"{item1} --> {item2}"}
+            )
+        i += 1
+    return differences
+
+
 async def notify_change(current_element: list, element: list, sheet_name):
     workers = await worker_table.get_authorized(
         [WorkerField.FULL_NAME.value, WorkerField.TELEGRAM_ID.value]
     )
     telegram_id = workers[current_element.pop(0)]
+    differences = get_differences(current_element.copy(), element.copy())
     await bot.send_message(
         telegram_id,
-        f"<b>Ваше расписание на {sheet_name} изменилось!</b>\n"
-        f"<b>Текущее расписание:</b>\n\n"
+        f"<b>Ваше расписание на {sheet_name} изменилось!</b>\n\n"
+        f"<b>Изменения:</b>\n"
+        f"<pre>{make_form(differences, ': ')}</pre>\n"
+        f"<b>Текущее расписание:</b>\n"
         f"<pre>{make_form(dict(zip(constants.week_abbreviated, current_element)))}</pre>",
         parse_mode="HTML"
     )
